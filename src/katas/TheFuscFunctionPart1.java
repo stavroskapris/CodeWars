4e3c15909f03a09a32595d024e298c3906059c70
StavrosKapris <33174615+StavrosKapris@users.noreply.github.com>
2017-11-16 11:13:41 +0200

    new commit

---
+++ src/katas/TheFuscFunctionPart1.java
@@ -0,0 +1,32 @@
+/*The fusc function is defined recursively as follows:
+
+fusc(0) = 0
+fusc(1) = 1
+fusc(2n) = fusc(n)
+fusc(2n + 1) = fusc(n) + fusc(n + 1)
+
+Your job is to produce the code for the fusc function. 
+In this kata, your function will be tested with small 
+values of n,so you should not need to be concerned about 
+stack overflow or timeouts.*/
+package katas;
+
+public class TheFuscFunctionPart1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int fusc(int n) {
+
+		if (n == 0 || n == 1)
+			return n;
+
+		if (n % 2 == 0)
+			return fusc(n / 2);
+
+		return fusc(n / 2) + fusc(n / 2 + 1);
+
+	}
+}
